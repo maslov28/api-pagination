d968798b281c248488f2e05eff2a71779532e6bb
maslov28 <artem.fatefulchoice@mail.ru>
2022-05-22 00:24:58 +0300

    used header

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 const btn = document.querySelector('.btn');
 let out = document.querySelector('.persons');
 let index = 1;
+let count;
 
 btn.addEventListener('click', addPerson);
+btn.style.display = 'none';
 
 addPerson();
 
 function addPerson() {
   fetch(`https://jsonplaceholder.typicode.com/users?_page=${index}&_limit=1`)
-    .then(response => response.json())
+    .then(response => {
+      count = +response.headers.get('x-total-count');
+      return response.json();
+    })
     .then(json => {
-      if (json[0]) {
-        let card = document.createElement('div');
-        card.classList.add('card');
-        card.innerHTML = json[0].name + '<br>' + json[0].email;
-        out.append(card);
-        index++;
-      }
-      else {
+      btn.style.display = '';
+      if (index == count) {
         btn.style.display = 'none';
       }
+      console.log(index, count);
+      let card = document.createElement('div');
+      card.classList.add('card');
+      card.innerHTML = json[0].name + '<br>' + json[0].email;
+      out.append(card);
+      index++;
     })
 }
